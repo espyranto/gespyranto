6eef9b590fe04c373adb8879a8f58508121e13b9
John Kitchin <jkitchin@andrew.cmu.edu>
2021-07-20 14:04:18 -0400

    add setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+# Copyright 2015-2019 John Kitchin
+# (see accompanying license files for details).
+from setuptools import setup
+
+setup(name='gespyranto',
+      version='0.0.2',
+      description='gespyranto',
+      url='',
+      maintainer='John Kitchin',
+      maintainer_email='jkitchin@andrew.cmu.edu',
+      license='GPL',
+      platforms=['linux'],
+      packages=[],
+      setup_requires=[],
+      data_files=[],
+      install_requires=[],
+      long_description='''
+      ''')
+
+# (shell-command "python setup.py register") to setup user
+# to push to pypi - (shell-command "python setup.py sdist upload")
+
+
+# Set TWINE_USERNAME and TWINE_PASSWORD in .bashrc
+# python setup.py sdist bdist_wheel
+# twine upload dist/*
